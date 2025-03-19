c802b2c7e415c471c4a6427e75be3e5017bbbd79
dastarruer <ayushpramanik399@gmail.com>
2025-03-19 18:52:15 -0400

    Add Command enum to track which command is invoked

--- chapter-8/todo_list/src/main.rs
+++ chapter-8/todo_list/src/main.rs
@@ -1,7 +1,22 @@
 use std::io;
+use std::io::Write;
+
+enum Command {
+    Add,
+    Complete,
+    List,
+}
 
 fn main() {
+    print!("Commands:\n1) Add task\n2) Mark task completed\n3) List tasks\n: ");
+    std::io::stdout().flush().expect("Error flushing stdout, please try again.");
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Error, please try again.");
-    println!("{}", input);
+    io::stdin().read_line(&mut input).expect("Error reading input, please try again.");
+    
+    let choice = match input.trim() {
+        "1" => Some(Command::Add),
+        "2" => Some(Command::Complete),
+        "3" => Some(Command::List),
+        _ => None,
+    };
 }
