3d3413258860312cc86afa2961507f40cf1bdd31
dastarruer <ayushpramanik399@gmail.com>
2025-03-19 18:54:25 -0400

    Move reading user input to own function

--- chapter-8/todo_list/src/main.rs
+++ chapter-8/todo_list/src/main.rs
@@ -8,15 +8,27 @@ enum Command {
 }
 
 fn main() {
-    print!("Commands:\n1) Add task\n2) Mark task completed\n3) List tasks\n: ");
-    std::io::stdout().flush().expect("Error flushing stdout, please try again.");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Error reading input, please try again.");
-    
-    let choice = match input.trim() {
+    let choice = match read_user_input().trim() {
         "1" => Some(Command::Add),
         "2" => Some(Command::Complete),
         "3" => Some(Command::List),
         _ => None,
     };
+}
+
+// Print the commands and read the user's input
+fn read_user_input() -> String {
+    // Print commands
+    print!("Commands:\n1) Add task\n2) Mark task completed\n3) List tasks\n: ");
+    std::io::stdout()
+        .flush()
+        .expect("Error flushing stdout, please try again.");
+
+    // Read user input
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Error reading input, please try again.");
+
+    input
 }
