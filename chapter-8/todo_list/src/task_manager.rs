e567deaba269a5a01c4c141d4f8a7cdfc4c4cc09
dastarruer <ayushpramanik399@gmail.com>
2025-03-19 20:37:54 -0400

    Add function to initialize tasks.json

--- chapter-8/todo_list/src/task_manager.rs
+++ chapter-8/todo_list/src/task_manager.rs
@@ -1,15 +1,48 @@
+use std::fs;
+
 pub struct TaskManager {
-    // The file to which json data will be stored
+    // The file to which JSON data will be stored
     filename: String,
-    // Stores the tasks json data
-    tasks: String,
+
+    // Stores the tasks JSON data
+    pub tasks: String,
 }
 
 impl TaskManager {
     pub fn new(filename: String) -> Self {
-        TaskManager {
+        let mut task_manager = TaskManager {
             filename,
-            tasks: "hello".to_string(),
-        }
+            // Initialize as empty first
+            tasks: "".to_string(),
+        };
+        task_manager.load_tasks();
+        task_manager
+    }
+
+    // Initialize tasks.JSON with an empty JSON object
+    fn init_tasks_file(&self) -> String {
+        println!("Tasks not found, initializing...");
+
+        let json = "{}";
+        fs::write(&self.filename, &json).expect("Error, railed to create file");
+
+        json.to_string()
+    }
+    
+    // Read tasks.json into self.tasks
+    fn load_tasks(&mut self) {
+        self.tasks = match fs::read_to_string(&self.filename) {
+            Ok(data) => {
+                match serde_json::from_str::<serde_json::Value>(&data) {
+                    Ok(_) => data,
+                    Err(_) => {
+                        eprintln!("Unable to load data, file may be corrupt.");
+                        "{}".to_string() // Return default empty JSON
+                    }
+                }
+            }
+            // If an error is thrown because there is no tasks.json file, initialize tasks.json
+            Err(_) => self.init_tasks_file()
+        };
     }
 }
