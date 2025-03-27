307ba68045bd77878a7d8c342353f97cb74d659f
dastarruer <ayushpramanik399@gmail.com>
2025-03-27 06:46:05 -0400

    Put main logic in while loop

--- chapter-8/todo_list/src/main.rs
+++ chapter-8/todo_list/src/main.rs
@@ -2,9 +2,7 @@ mod task;
 mod task_manager;
 mod utils;
 
-use task_manager::TaskManager;
-use utils::get_input;
-
+use utils::get_command;
 
 enum Command {
     Add,
@@ -14,27 +12,7 @@ enum Command {
 }
 
 fn main() {
-    let mut task_manager = TaskManager::new("./tasks.json".to_string());
-
-    // Get user command
-    let command = match get_input(
-        "Commands:\n1) Add task\n2) Edit task\n3) Complete task\n4) List tasks\n: ",
-    )
-    .parse::<u8>() // Cast to a uint so that the compiler doesn't get mad about comparing String to &str
-    .ok()
-    {
-        Some(1) => Some(Command::Add),
-        Some(2) => Some(Command::Edit),
-        Some(3) => Some(Command::Complete),
-        Some(4) => Some(Command::List),
-        _ => None,
-    };
-
-    match command {
-        Some(Command::Add) => task_manager.add_task(),
-        Some(Command::List) => task_manager.list_tasks(),
-        Some(Command::Complete) => task_manager.complete_task(),
-        Some(Command::Edit) => task_manager.edit_tasks(),
-        None => eprintln!("Error, invalid command. Please try again"),
+    loop {
+        get_command();
     }
 }
--- chapter-8/todo_list/src/utils.rs
+++ chapter-8/todo_list/src/utils.rs
@@ -1,6 +1,9 @@
 use std::io;
 use std::io::Write;
 
+use crate::task_manager::TaskManager; // ✅ Import from crate root
+use crate::Command;
+
 // Function to prompt user and read input
 pub fn get_input(prompt: &str) -> String {
     print!("{}", prompt);
@@ -14,4 +17,31 @@ pub fn get_input(prompt: &str) -> String {
         .expect("Error reading input, please try again.");
 
     input.trim().to_string()
+}
+
+// Function to get the user's command
+pub fn get_command() {
+    let mut task_manager = TaskManager::new("./tasks.json".to_string());
+
+    // Get user command
+    let command = match get_input(
+        "Commands:\n1) Add task\n2) Edit task\n3) Complete task\n4) List tasks\n: ",
+    )
+    .parse::<u8>() // Parse as integer
+    .ok()
+    {
+        Some(1) => Some(Command::Add),
+        Some(2) => Some(Command::Edit),
+        Some(3) => Some(Command::Complete),
+        Some(4) => Some(Command::List),
+        _ => None,
+    };
+
+    match command {
+        Some(Command::Add) => task_manager.add_task(),
+        Some(Command::List) => task_manager.list_tasks(),
+        Some(Command::Complete) => task_manager.complete_task(),
+        Some(Command::Edit) => task_manager.edit_tasks(),
+        None => eprintln!("Error, invalid command. Please try again"),
+    }
 }
