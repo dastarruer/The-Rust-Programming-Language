3d22481d2a3ac825e071af06052ef988bcf86b90
dastarruer <ayushpramanik399@gmail.com>
2025-03-30 14:48:34 -0400

    Add comments

--- chapter-12/minigrep/src/lib.rs
+++ chapter-12/minigrep/src/lib.rs
@@ -1,6 +1,9 @@
 use std::io::Read;
 use std::fs::File;
 
+/**
+Stores the commandline arguments given by the user.
+*/
 pub struct Config {
     query: String,
     filename: String,
@@ -8,9 +11,11 @@ pub struct Config {
 
 impl Config {
     pub fn new(args: &Vec<String>) -> Result<Self, &'static str> {
+        // If there are less than three arguments, the program cannot run
         if args.len() < 3 {
             return Err("Not enough arguments.");
         }
+        // TODO: Remove cloning here
         Ok(Config {
             query: args[1].clone(),
             filename: args[2].clone(),
@@ -18,6 +23,7 @@ impl Config {
     }
 }
 
+/// Run main logic
 pub fn run(config: Config) {
     println!("{}", config.filename);
     let mut f = File::open(config.filename).expect("Unable to find {config.filename}");
@@ -28,11 +34,14 @@ pub fn run(config: Config) {
     println!("{:?}", search(&config.query, &content));
 }
 
+/// Search for a word in a given file's content
 fn search<'a>(query: &'a str, content: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new(); 
     
+    // Go through each line
     for line in content.lines() {
         if line.contains(query) {
+            // Add the line to the results
             results.push(line);
         }
     }
