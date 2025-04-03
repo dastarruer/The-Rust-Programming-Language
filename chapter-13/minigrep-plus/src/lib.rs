549ac3242932a6d0f7f796cd5669e9cd6c4cb161
dastarruer <ayushpramanik399@gmail.com>
2025-04-02 20:25:58 -0400

    Init project

---
+++ chapter-13/minigrep-plus/src/lib.rs
@@ -0,0 +1,156 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+/// Stores the commandline arguments given by the user.
+pub struct Config {
+    queries: Vec<String>,
+    filename: String,
+    case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(args: &Vec<String>) -> Result<Self, &'static str> {
+        let num_args = args.len();
+
+        // If there are less than three arguments, the program cannot run
+        if num_args < 3 {
+            return Err("Not enough arguments.");
+        }
+
+        let queries = args[1..num_args - 1]
+            .iter()
+            .map(|s| s.to_string())
+            .collect();
+        let filename = args[num_args - 1].clone();
+
+        /*
+        If there is no env variable called CASE_INSENSITIVE, .is_err will return true, meaning that we should search with case sensitivity.
+        */
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Config {
+            queries,
+            filename,
+            case_sensitive,
+        })
+    }
+}
+
+/// Run main logic
+pub fn run(config: &Config) -> Result<(), std::io::Error> {
+    // Read file content
+    let content = read_from_file(&config.filename)?;
+
+    let query_refs = config.queries.iter().map(|s| s.as_str()).collect();
+    let results;
+
+    // Search for word in file and print results
+    if config.case_sensitive == true {
+        results = search(query_refs, &content);
+    } else {
+        results = search_case_insensitive(query_refs, &content)
+    }
+    print_results(results);
+
+    Ok(())
+}
+
+/// Return file content
+fn read_from_file(filename: &str) -> Result<String, std::io::Error> {
+    let mut f = File::open(filename)?;
+
+    let mut content = String::new();
+    f.read_to_string(&mut content)?;
+
+    Ok(content)
+}
+
+/// Search for a word in a given file's content
+fn search<'a>(queries: Vec<&'a str>, content: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    // Go through each line
+    for line in content.lines() {
+        for query in queries.iter() {
+            if line.contains(query) {
+                // Add the line to the results
+                results.push(line);
+            }
+        }
+    }
+
+    results
+}
+/// Search for a word in a given file's content, disregarding case
+fn search_case_insensitive<'a>(queries: Vec<&'a str>, content: &'a str) -> Vec<&'a str> {
+    let queries: Vec<_> = queries.into_iter().map(|s| s.to_lowercase()).collect();
+    let mut results = Vec::new();
+
+    // Go through each line
+    for line in content.lines() {
+        for query in queries.iter() {
+            if line.to_lowercase().contains(query) {
+                // Add the line to the results
+                results.push(line);
+            }
+        }
+    }
+
+    results
+}
+
+fn print_results(results: Vec<&str>) {
+    for result in results.iter() {
+        println!("{}", result);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_query() {
+        let query = vec!["duct"];
+        let content = "Rust: \nsafe, fast, productive.\nPick three.
+        ";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, content))
+    }
+
+    #[test]
+    fn one_query_case_insensitive() {
+        let query = vec!["dUcT"];
+        let content = "Rust: \nsafe, fast, productive.\nPick three.
+        ";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search_case_insensitive(query, content)
+        )
+    }
+
+    #[test]
+    fn multiple_queries_case_insensitive() {
+        let query = vec!["rUst", "dUcT"];
+        let content = "Rust: \nsafe, fast, productive.\nPick three.
+        ";
+
+        assert_eq!(
+            vec!["Rust: ", "safe, fast, productive."],
+            search_case_insensitive(query, content)
+        )
+    }
+
+    #[test]
+    fn multiple_queries() {
+        let query = vec!["Rust", "duct"];
+        let content = "Rust: \nsafe, fast, productive.\nPick three.
+        ";
+
+        assert_eq!(
+            vec!["Rust: ", "safe, fast, productive."],
+            search(query, content)
+        )
+    }
+}
---
+++ chapter-13/minigrep-plus/src/main.rs
@@ -0,0 +1,20 @@
+use std::process;
+use std::env;
+
+use minigrep::Config;
+use minigrep::run;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err: &str| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    run(&config).unwrap_or_else(|err| {
+        eprintln!("Problem running program: {}", err);
+        process::exit(1);
+    });
+}
+
