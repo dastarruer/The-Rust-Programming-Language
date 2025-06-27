268a549c3403489582365836e5bc6211f819792d
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-06-27 14:20:05 -0400

    Modify tests to take only 1d arrays as input

--- chapter-10/matrix_math/src/lib.rs
+++ chapter-10/matrix_math/src/lib.rs
@@ -2,12 +2,14 @@
 #[derive(PartialEq)]
 #[allow(dead_code)]
 struct Matrix<T> {
-    content: Vec<Vec<T>>,
+    rows: i8,
+    cols: i8,
+    values: Vec<Vec<T>>,
 }
 
 impl<T: Clone + Copy> Matrix<T> {
     #[allow(dead_code)]
-    fn new<'a>(cols: i8, values: Vec<T>) -> Result<Matrix<T>, &'static str> {
+    fn new<'a>(rows: i8, cols: i8, values: Vec<T>) -> Result<Matrix<T>, &'static str> {
         // Check if there are more values than there are columns
         if values.len() % cols as usize != 0 {
             return Err("Number of values exceeds the number of columns available.")
@@ -46,7 +48,7 @@ impl<T: Clone + Copy> Matrix<T> {
         
         // Return the final matrix
         Ok(Matrix {
-            content: final_values,
+            values: final_values,
         })
     }
 }
@@ -58,25 +60,22 @@ mod tests {
     mod init_matrix {
         use crate::Matrix;
 
-        
         #[test]
         fn init_matrix_i32() {
-            let matrix = Matrix::<i32>::new(3, vec![1, 2, 3, 4, 5, 6]).unwrap();
-            let expected = vec![
-                vec![1, 2, 3],
-                vec![4, 5, 6],
-            ];
-            assert_eq!(matrix.content, expected);
+            let values = vec![1, 2, 3, 4, 5, 6];
+            let matrix = Matrix::<i32>::new(2, 3, &values).unwrap();
+            assert_eq!(matrix.rows, 2);
+            assert_eq!(matrix.cols, 3);
+            assert_eq!(matrix.values, values);
         }
     
         #[test]
         fn init_matrix_f64() {
-            let matrix = Matrix::<f64>::new(3, vec![1.25, 2.25, 3.25, 4.25, 5.25, 6.25]).unwrap();
-            let expected = vec![
-                vec![1.25, 2.25, 3.25],
-                vec![4.25, 5.25, 6.25],
-            ];
-            assert_eq!(matrix.content, expected);
+            let values = vec![1, 2, 3, 4, 5, 6];
+            let matrix = Matrix::<i32>::new(2, 3, &values).unwrap();
+            assert_eq!(matrix.rows, 2);
+            assert_eq!(matrix.cols, 3);
+            assert_eq!(matrix.values, values);
         }
     
         #[test]
