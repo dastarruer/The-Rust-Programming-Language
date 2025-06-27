5a73a9f2d1af2592122dfda5efa1ae8dbaf17505
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-06-27 11:15:32 -0400

    Add dummy return to Matrix::new

--- chapter-10/matrix_math/src/lib.rs
+++ chapter-10/matrix_math/src/lib.rs
@@ -3,8 +3,17 @@ struct Matrix<T> {
 }
 
 impl<T> Matrix<T> {
-    fn new(rows: i32, cols: i32, content: Vec<T>) -> Matrix<T> {
-        let final_content = Vec::new();
+    fn new(rows: i32, cols: i32, content: Vec<i32>) -> Matrix<i32> {
+        let _final_content: Vec<i32> = Vec::new();
+
+        for (i, value) in content.iter().enumerate() {
+
+        }
+        
+        // Silence the compiler for now with dummy data
+        Matrix {
+            content: vec![vec![0, 0, 0], vec![0, 0, 0]],
+        }
     }
 }
 
@@ -14,7 +23,7 @@ mod tests {
 
     #[test]
     fn init_matrix() {
-        let matrix = Matrix::new(2, 3, vec![1, 2, 3, 4, 5, 6]);
+        let matrix = Matrix::<i32>::new(2, 3, vec![1, 2, 3, 4, 5, 6]);
         let expected = vec![
             vec![1, 2, 3],
             vec![4, 5, 6],
