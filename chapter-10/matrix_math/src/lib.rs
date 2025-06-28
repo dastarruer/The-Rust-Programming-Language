f42fc13d94b8a0a8fc24cca15bd160352542ab04
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-06-28 10:18:10 -0400

    Lint files

--- chapter-10/matrix_math/src/lib.rs
+++ chapter-10/matrix_math/src/lib.rs
@@ -1,5 +1,4 @@
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 #[allow(dead_code)]
 struct Matrix<T> {
     rows: i8,
@@ -7,19 +6,17 @@ struct Matrix<T> {
     values: Vec<T>,
 }
 
-impl<T: Clone + Copy + std::ops::Add<Output = T>> Matrix<T>{
+impl<T: Clone + Copy + std::ops::Add<Output = T>> Matrix<T> {
     #[allow(dead_code)]
     fn new(rows: i8, cols: i8, values: Vec<T>) -> Result<Matrix<T>, &'static str> {
         // Check if there are more values than there are columns
         if values.len() % cols as usize != 0 {
-            return Err("Cannot create matrix: leftover values that can't fill an entire row are present.")
+            return Err(
+                "Cannot create matrix: leftover values that can't fill an entire row are present.",
+            );
         }
         // Return the final matrix
-        Ok(Matrix {
-            rows,
-            cols,
-            values,
-        })
+        Ok(Matrix { rows, cols, values })
     }
 
     // Check that two matrices have the same dimensions
@@ -32,7 +29,7 @@ impl<T: Clone + Copy + std::ops::Add<Output = T>> Matrix<T>{
     fn add(a: Matrix<T>, b: Matrix<T>) -> Result<Matrix<T>, &'static str> {
         // If the matrices are of different dimensions, then they cannot be added
         if !Self::is_same_dimensions(&a, &b) {
-            return Err("Cannot add matrices: Both matrices are of different dimensions.")
+            return Err("Cannot add matrices: Both matrices are of different dimensions.");
         }
 
         let mut new_values = Vec::new();
@@ -48,14 +45,13 @@ impl<T: Clone + Copy + std::ops::Add<Output = T>> Matrix<T>{
             values: new_values,
         })
     }
-
 }
 
 #[cfg(test)]
 mod tests {
     // use super::*;
-    
-    // Initializing matrices 
+
+    // Initializing matrices
     mod init_matrix {
         use crate::Matrix;
 
@@ -78,21 +74,31 @@ mod tests {
             assert_eq!(matrix.cols, 3);
             assert_eq!(matrix.values, values);
         }
-    
+
         // Check that invalide matrices throw an error
         #[test]
         fn init_matrix_invalid() {
             let error = Matrix::<f64>::new(2, 3, vec![1.25, 2.25, 3.25, 4.25, 5.25, 6.0, 7.25]);
-            assert_eq!(error, Err("Cannot create matrix: leftover values that can't fill an entire row are present."));
+            assert_eq!(
+                error,
+                Err(
+                    "Cannot create matrix: leftover values that can't fill an entire row are present."
+                )
+            );
 
             let error = Matrix::<f64>::new(2, 3, vec![1.25, 2.25, 3.25, 4.25, 5.25]);
-            assert_eq!(error, Err("Cannot create matrix: leftover values that can't fill an entire row are present."));
+            assert_eq!(
+                error,
+                Err(
+                    "Cannot create matrix: leftover values that can't fill an entire row are present."
+                )
+            );
         }
     }
 
     mod validate_dimensions {
         use crate::Matrix;
-        
+
         // Compare two matrices of same dimensions
         #[test]
         fn compare_same_dimensions() {
@@ -162,6 +168,10 @@ mod tests {
             assert_eq!(c.cols, 2);
         }
 
+        // Add matrices of different types
+        #[test]
+        fn add_matrix_different_types() {}
+
         // Add matrices of different dimensions (invalid)
         #[test]
         fn add_matrix_invalid() {
@@ -169,7 +179,10 @@ mod tests {
             let b = Matrix::new(2, 2, vec![5.0, 3.0, 2.0, 1.0]).unwrap();
             let error = Matrix::add(a, b);
 
-            assert_eq!(error, Err("Cannot add matrices: Both matrices are of different dimensions."));
+            assert_eq!(
+                error,
+                Err("Cannot add matrices: Both matrices are of different dimensions.")
+            );
         }
     }
 }
