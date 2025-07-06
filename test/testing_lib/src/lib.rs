60e1453407f88e5a2556a026dbb23340609155d9
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-06 11:15:55 -0400

    add test that passes all the time

--- test/testing_lib/src/lib.rs
+++ test/testing_lib/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
+        let result = 4;
         assert_eq!(result, 4);
     }
 }
