40f472f0bc45ba236f3279702562ab56a71dfc76
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-06 11:19:37 -0400

    Remove unused import and dead code warnings

--- test/testing_lib/flake.nix
+++ test/testing_lib/flake.nix
@@ -48,6 +48,9 @@
         env = {
           # Required by rust-analyzer
           RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+
+          # Remove dead code and unused import warnings, not necessary during testing
+          RUSTFLAGS = "-A unused-imports -A dead-code";
         };
       };
     });
