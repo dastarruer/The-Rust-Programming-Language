6da7f635ed96740edcb57a29a064b0a2b2a5cd08
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-08 10:58:18 -0400

    Implement MyRc

--- chapter-15/my_rc/src/lib.rs
+++ chapter-15/my_rc/src/lib.rs
@@ -1,18 +1,52 @@
-/// A simple implementation of Rc<T>
-pub struct MyRc<T> {
-    /// Stores the value of the pointer in a Box 
-    value: Box<T>,
+use std::alloc::{Layout, alloc, dealloc};
+use std::ops::Deref;
+use std::ptr;
+
+/// The heap-allocated structure that a MyRc will point to
+struct InnerRc<T> {
+    /// Stores the value of the pointer in a Box
+    value: T,
 
     /// Keeps track of the number of references made to the pointer
-    strong_count: i32,
+    strong_count: usize,
+}
+
+/// A simple implementation of Rc<T>
+pub struct MyRc<T> {
+    ptr: *mut InnerRc<T>,
 }
 
 impl<T> MyRc<T> {
     fn new(value: T) -> Self {
-        MyRc {
-            value: Box::new(value),
+        let ptr = Box::new(InnerRc {
+            value,
             strong_count: 1,
+        });
+        let ptr = Box::into_raw(ptr);
+
+        MyRc { ptr }
+    }
+}
+
+impl<T> Drop for MyRc<T> {
+    fn drop(&mut self) {
+        unsafe {
+            // Remove one from reference count
+            (*self.ptr).strong_count -= 1;
+
+            // If there are no more references to the data, drop it
+            if (*self.ptr).strong_count == 0 {
+                drop(Box::from_raw(self.ptr));
+            }
         }
+    }
+}
+
+impl<T> Deref for MyRc<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        unsafe { &(*self.ptr).value }
     }
 }
 
@@ -23,7 +57,6 @@ mod tests {
     #[test]
     fn create_new_rc() {
         let my_rc = MyRc::new(15);
-        assert_eq!(*my_rc.value, 15);
-        assert_eq!(my_rc.strong_count, 1);
+        assert_eq!(*my_rc, 15);
     }
 }
