d173d2881cf3a05026e709e8228d8801cd27249f
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-10 12:32:14 -0400

    implement simple word counter

--- chapter-16/word_count/src/lib.rs
+++ chapter-16/word_count/src/lib.rs
@@ -4,7 +4,19 @@ pub struct WordCounter<'a> {
 
 impl<'a> WordCounter<'a> {
     fn count_words(&self) -> i32 {
-        15
+        // Check if text is empty
+        if self.text.is_empty() {
+            return 0;
+        }
+
+        let mut counter = 1;
+        for c in self.text.chars() {
+            if c == ' ' {
+                counter += 1;
+            }
+        }
+
+        counter
     }
 }
 
@@ -14,7 +26,23 @@ mod tests {
 
     #[test]
     fn test_count_words() {
-        let wc = WordCounter { text: "hello world" };
+        let wc = WordCounter {
+            text: "hello world",
+        };
         assert_eq!(wc.count_words(), 2);
+    }
+
+    #[test]
+    fn test_dashes() {
+        let wc = WordCounter {
+            text: "hello-world",
+        };
+        assert_eq!(wc.count_words(), 1);
+    }
+
+    #[test]
+    fn test_empty_string() {
+        let wc = WordCounter { text: "" };
+        assert_eq!(wc.count_words(), 0);
     }
 }
