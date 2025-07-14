9513a46f58557a40f9a4ba24117ae3b6538aa100
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-14 10:39:43 -0400

    Add get_longest_word

--- chapter-16/word_stats/src/lib.rs
+++ chapter-16/word_stats/src/lib.rs
@@ -9,9 +9,18 @@ impl<'a> TextAnalyzer<'a> {
             return 0;
         }
 
-        self.text.split_whitespace().collect::<Vec<_>>().len()
+        self.text.split_whitespace().collect::<Vec<&str>>().len()
     }
 
+    fn get_longest_word(&self) -> &str {
+        let mut longest_word: &str = "";
+        for word in self.text.split_whitespace().collect::<Vec<&str>>() {
+            if word.len() > longest_word.len() {
+                longest_word = word;
+            }
+        }
+        longest_word
+    }
 }
 
 #[cfg(test)]
@@ -19,11 +28,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_count_words() {
+    fn test_get_word_count() {
         let wc = TextAnalyzer {
             text: "hello world",
         };
-        assert_eq!(wc.count_words(), 2);
+        assert_eq!(wc.get_word_count(), 2);
     }
 
     #[test]
@@ -31,12 +40,25 @@ mod tests {
         let wc = TextAnalyzer {
             text: "hello-world",
         };
-        assert_eq!(wc.count_words(), 1);
+        assert_eq!(wc.get_word_count(), 1);
     }
 
     #[test]
     fn test_empty_string() {
         let wc = TextAnalyzer { text: "" };
-        assert_eq!(wc.count_words(), 0);
+        assert_eq!(wc.get_word_count(), 0);
+    }
+
+    #[test]
+    fn test_longest_word() {
+        let wc = TextAnalyzer {
+            text: "hello-world",
+        };
+        assert_eq!(wc.get_longest_word(), "hello-world");
+
+        let wc = TextAnalyzer {
+            text: "catastrophe hello there when alskdfjalsdkfjalksdfj",
+        };
+        assert_eq!(wc.get_longest_word(), "alskdfjalsdkfjalksdfj");
     }
 }
