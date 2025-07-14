bc10013220489b6da942294499905d1abb2a18d5
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-14 10:59:47 -0400

    Add get_word_stats

--- chapter-16/word_stats/src/lib.rs
+++ chapter-16/word_stats/src/lib.rs
@@ -3,6 +3,19 @@ pub struct TextAnalyzer<'a> {
 }
 
 impl<'a> TextAnalyzer<'a> {
+    /// Return the stats of a given &str
+    fn get_word_stats(&self) -> String {
+        let word_count = TextAnalyzer::get_word_count(self.text);
+        let longest_word = TextAnalyzer::get_longest_word(self.text);
+
+        format!(
+            r"Stats:
+        Word count: {}
+        Longest word: {}",
+            word_count, longest_word
+        )
+    }
+
     /// Return the word count of a given &str
     fn get_word_count(text: &str) -> usize {
         // Check if text is empty
@@ -27,30 +40,57 @@ impl<'a> TextAnalyzer<'a> {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
-    #[test]
-    fn test_get_word_count() {
-        assert_eq!(TextAnalyzer::get_word_count("hello world"), 2);
+    mod word_stats {
+        use super::super::*;
+
+        #[test]
+        fn test_word_stats() {
+            let text = TextAnalyzer {
+                text: "Hello worlds",
+            };
+
+            assert_eq!(
+                text.get_word_stats(),
+                r"Stats:
+        Word count: 2
+        Longest word: worlds"
+            )
+        }
     }
 
-    #[test]
-    fn test_dashes() {
-        assert_eq!(TextAnalyzer::get_word_count("hello-world"), 1);
+    mod word_count {
+        use super::super::*;
+
+        #[test]
+        fn test_get_word_count() {
+            assert_eq!(TextAnalyzer::get_word_count("hello world"), 2);
+        }
+
+        #[test]
+        fn test_dashes() {
+            assert_eq!(TextAnalyzer::get_word_count("hello-world"), 1);
+        }
+
+        #[test]
+        fn test_empty_string() {
+            assert_eq!(TextAnalyzer::get_word_count(""), 0);
+        }
     }
 
-    #[test]
-    fn test_empty_string() {
-        assert_eq!(TextAnalyzer::get_word_count(""), 0);
-    }
+    mod longest_word {
+        use super::super::*;
 
-    #[test]
-    fn test_longest_word() {
-        assert_eq!(TextAnalyzer::get_longest_word("hello-world"), "hello-world");
+        #[test]
+        fn test_longest_word() {
+            assert_eq!(TextAnalyzer::get_longest_word("hello-world"), "hello-world");
 
-        assert_eq!(
-            TextAnalyzer::get_longest_word("catastrophe hello there when alskdfjalsdkfjalksdfj"),
-            "alskdfjalsdkfjalksdfj"
-        );
+            assert_eq!(
+                TextAnalyzer::get_longest_word(
+                    "catastrophe hello there when alskdfjalsdkfjalksdfj"
+                ),
+                "alskdfjalsdkfjalksdfj"
+            );
+        }
     }
 }
