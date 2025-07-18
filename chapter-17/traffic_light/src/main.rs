930237d9ae2f45e8c5a228d9b4d280aa4d98fd97
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-18 12:03:19 -0400

    Add traffic lights

--- chapter-17/traffic_light/src/main.rs
+++ chapter-17/traffic_light/src/main.rs
@@ -1,3 +1,43 @@
+pub struct Light {
+    state: Option<Box<dyn State>>,
+}
+
+impl Light {
+    pub fn new() -> Self {
+        Light {
+            state: Some(Box::new(Red {})),
+        }
+    }
+}
+
+trait State {
+    fn change_light(self: Box<Self>) -> Box<dyn State>;
+}
+
+struct Red {}
+
+struct Green {}
+
+struct Yellow {}
+
+impl State for Red {
+    fn change_light(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Green {})
+    }
+}
+
+impl State for Green {
+    fn change_light(self: Box<Self>) -> Box<dyn State> {
+         Box::new(Yellow {})
+    }
+}
+
+impl State for Yellow {
+    fn change_light(self: Box<Self>) -> Box<dyn State> {
+         Box::new(Red {})
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
